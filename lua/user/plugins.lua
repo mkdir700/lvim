57801317bce23e9a7a520471102e72391adf8357
mkdir700 <mkdir700@gmail.com>
2023-05-25 22:29:57 +0800

    更新 github 主题

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -428,26 +428,23 @@ M.config = function()
 		},
 		{
 			"projekt0n/github-nvim-theme",
-			tag = "v0.0.7",
+			tag = "v1.0.0",
 			config = function()
 				require("github-theme").setup({
 					-- dark/dimmed/dark_default/dark_colorblind/light/light_default/light_colorblind
-					theme_style = "dark",
+					theme_style = "dimmed",
 					function_style = "italic",
+					keyword_style = "italic",
 					sidebars = { "qf", "vista_kind", "terminal", "packer" },
-					-- Change the "hint" color to the "orange" color, and make the "error" color bright red
-					-- colors = { hint = "orange", error = "#ff0000" },
-					-- Overwrite the highlight groups
-					-- overrides = function(c)
-					-- 	return {
-					-- 		htmlTag = { fg = c.red, bg = "#282c34", sp = c.hint, style = "underline" },
-					-- 		DiagnosticHint = { link = "LspDiagnosticsDefaultHint" },
-					-- 		-- this will remove the highlight groups
-					-- 		TSField = {},
-					-- 	}
-					-- end,
+					options = {
+						inverse = { -- Inverse highlight for different types
+							match_paren = true,
+							visual = true,
+							search = true,
+						},
+					},
 				})
-				vim.cmd([[colorscheme github_dark]])
+				vim.cmd("colorscheme github_dimmed")
 			end,
 			cond = function()
 				local _time = os.date("*t")
