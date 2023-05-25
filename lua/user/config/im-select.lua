fd5ce693ac340fc647e1e95168432456cb0b2b5a
mkdir700 <mkdir700@gmail.com>
2023-05-25 22:30:21 +0800

    加入操作系统逻辑判断函数

--- lua/user/config/im-select.lua
+++ lua/user/config/im-select.lua
@@ -1,13 +1,36 @@
 local M = {}
 
 M.config = function()
+	local function im_select()
+		-- 判断当前系统
+		local os = vim.loop.os_uname().sysname
+		if os == "Darwin" then
+			return "com.apple.keylayout.ABC"
+		elseif os == "Linux" then
+			return "1033"
+		elseif os == "Windows" then
+			return "1033"
+		end
+	end
+
+	local function command()
+		local os = vim.loop.os_uname().sysname
+		if os == "Darwin" then
+			return "im-select"
+		elseif os == "Linux" then
+			return "im-select"
+		elseif os == "Windows" then
+			return "im-select.exe"
+		end
+	end
+
 	require("im_select").setup({
 
 		-- IM will be set to `default_im_select` in `normal` mode(`EnterVim` or `InsertLeave`)
 		-- For Windows/WSL, default: "1033", aka: English US Keyboard
 		-- For macOS, default: "com.apple.keylayout.ABC", aka: US
 		-- You can use `im-select` in cli to get the IM's name you preferred
-		default_im_select = "com.apple.keylayout.ABC",
+		default_im_select = im_select(),
 
 		-- Set to 1 if you don't want restore IM status when `InsertEnter`
 		set_previous_events = 1,
@@ -16,7 +39,7 @@ M.config = function()
 		-- e.g. 'im-select' or '/usr/local/bin/im-select'
 		-- For Windows/WSL, default: "im-select.exe"
 		-- For macOS, default: "im-select"
-		default_command = "im-select",
+		default_command = command(),
 	})
 end
 
