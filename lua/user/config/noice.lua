d5feb689156cd9d9cdca4a01ffb679ada452a342
mkdir700 <mkdir700@gmail.com>
2023-05-25 23:05:01 +0800

    update noice

--- lua/user/config/noice.lua
+++ lua/user/config/noice.lua
@@ -14,10 +14,9 @@ M.config = function()
 			},
 		},
 		messages = {
-			enabled = true, -- enables the Noice messages UI
-			-- view = "mini", -- default view for messages
-			view = false, -- default view for messages
-			view_error = "mini", -- view for errors
+			enabled = false, -- enables the Noice messages UI
+			view = "mini", -- default view for messages
+			view_error = "notify", -- view for errors
 			view_warn = "notify", -- view for warnings
 			view_history = "messages", -- view for :messages
 			view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
@@ -49,7 +48,7 @@ M.config = function()
 			command_palette = true, -- position the cmdline and popupmenu together
 			long_message_to_split = true, -- long messages will be sent to a split
 			inc_rename = false, -- enables an input dialog for inc-rename.nvim
-			lsp_doc_border = false, -- add a border to hover docs and signature help
+			lsp_doc_border = true, -- add a border to hover docs and signature help
 		},
 		views = {
 			merged_notify = {
@@ -113,7 +112,7 @@ M.config = function()
 			DEBUG = " ",
 			TRACE = " ",
 		},
-		-- background_colour = "#000000",
+		background_colour = "#ccc",
 	})
 end
 
