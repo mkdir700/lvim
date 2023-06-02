4b236671cc7eb9f58ee6c7cd2818202c0c6591d6
mkdir700 <mkdir700@gmail.com>
2023-06-02 23:54:50 +0800

    update lsp_signature

--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -36,14 +36,15 @@ M.config = function()
 	require("lvim.lsp").common_on_attach = function(client, bufnr)
 		require("lsp_signature").on_attach({
 			doc_lines = 10,
-			floating_window = false,
-			floating_window_above_cur_line = false,
+			floating_window = true,
+			floating_window_above_cur_line = true,
 			hint_enable = true,
 			hint_prefix = "🦉: ",
 			extra_trigger_chars = { "(", ",", "=" }, -- Array of extra characters that will trigger signature completion, e.g., {"(", ","}
 			zindex = 1002, -- by default it will be on top of all floating windows, set to 50 send it to bottom
 			toggle_key = "<C-k>",
-			"/home/",
+			hi_parameter = "LspSignatureActiveParameter", -- how your parameter will be highlight
+			shadow_guibg = "Green", -- if you using shadow as border use this set the color e.g. 'Green' or '#121315'
 		})
 		common_on_attach(client, bufnr)
 	end
